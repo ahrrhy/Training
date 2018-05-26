6b8772fbefac31f76ac8424ab2bbe461c70ecb4a
ahrrhy <ahrrhy@gmail.com>
2018-05-26 19:59:53 +0300

    slide forward with .animate()

--- dest/js/build.js
+++ dest/js/build.js
@@ -10397,37 +10397,49 @@ return jQuery;
            if (options.transition === 'slide') {
                var wrap = '<div id="slider-wrap__slide"></div>';
                $this.wrap(wrap);
-               var items = $this.find('.slide'),
-                   activeSlide = items.filter('.active'),
-                   nextSlide = activeSlide.next(),
-                   prevSlide = activeSlide.prev(),
-                   firstSlide = items.first(),
-                   lastSlide = items.last(),
-                   wrapper = $('.slider-box'),
-                   sliderOffset = wrapper.offset().left,
-                   reqPos = 0;
-               slide();
+               var currentSliderPosition = 0;
+
+               function nextSlideMove() {
+                   var items = $this.find('li'),
+                       activeSlide = items.filter('.active'),
+                       nextSlide = activeSlide.next(),
+                       firstSlide = items.first();
+                   if (nextSlide.length) {
+                       $this.animate({right: currentSliderPosition + nextSlide.width()}, options.speed);
+                       currentSliderPosition += nextSlide.width();
+                       console.log(nextSlide.length);
+                       activeSlide.removeClass('active').next().addClass('active');
+                   } else {
+                       firstSlide.clone().appendTo($this);
+                       $this.animate({right: currentSliderPosition + nextSlide.width()}, options.speed);
+                       activeSlide.removeClass('active').next().addClass('active');
+                   }
+               }
+
+               function prevSlideMove() {
+                   var items = $this.find('li'),
+                       activeSlide = items.filter('.active'),
+                       nextSlide = activeSlide.next(),
+                       prevSlide = activeSlide.prev(),
+                       firstSlide = items.first(),
+                       lastSlide = items.last(),
+                       wrapper = $('#slider-wrap__slide');
+
+                   if (prevSlide.length) {
+                       return activeSlide.removeClass('active').prev().addClass('active');
+                   } else {
+                       lastSlide.clone().prependTo($this);
+                       lastSlide.remove();
+                       return activeSlide.removeClass('active').prev().addClass('active');
+                   }
+               }
 
                nextBtn.on('click', function() {
                    console.log('fg');
-                   if (nextSlide.length) {
-                       findReqPos(nextSlide);
-                       removeActive(nextSlide);
-                   } else {
-                       findReqPos(firstSlide);
-                       removeActive(prevSlide);
-                   }
-                   $this.css('left', '-=' + reqPos + 'px');
+                   nextSlideMove();
                });
                prevBtn.on('click', function() {
-                   if (prevSlide.length) {
-                       findReqPos(nextSlide);
-                       removeActive(nextSlide);
-                   } else {
-                       findReqPos(lastSlide);
-                       removeActive(prevSlide);
-                   }
-                   $this.css('left', '-=' + reqPos + 'px');
+                   prevSlideMove();
                });
            }
 
@@ -10436,19 +10448,10 @@ return jQuery;
                fade();
            }
 
-           function slide() {
-               console.log(nextBtn);
-
-           }
-
            function fade() {
 
            }
 
-           function findReqPos(slide) {
-               reqPos = slide.offset().left - sliderOffset;
-           }
-           
            function removeActive(reqSlide) {
                reqSlide.addClass('active').siblings().removeClass('active');
            }
