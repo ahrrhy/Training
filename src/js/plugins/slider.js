9c8dd517aece8de89318b36c65e4aed33f1c0ca7
ahrrhy <ahrrhy@gmail>
2018-05-26 21:56:46 +0300

    wrong way

--- src/js/plugins/slider.js
+++ src/js/plugins/slider.js
@@ -20,61 +20,15 @@
            if (options.transition === 'slide') {
                var wrap = '<div id="slider-wrap__slide"></div>';
                $this.wrap(wrap);
-               var items = $this.find('.slide'),
-                   activeSlide = items.filter('.active'),
-                   nextSlide = activeSlide.next(),
-                   prevSlide = activeSlide.prev(),
-                   firstSlide = items.first(),
-                   lastSlide = items.last(),
-                   wrapper = $('.slider-box'),
-                   sliderOffset = wrapper.offset().left,
-                   reqPos = 0;
-               slide();
 
-               nextBtn.on('click', function() {
-                   console.log('fg');
-                   if (nextSlide.length) {
-                       findReqPos(nextSlide);
-                       removeActive(nextSlide);
-                   } else {
-                       findReqPos(firstSlide);
-                       removeActive(prevSlide);
-                   }
-                   $this.css('left', '-=' + reqPos + 'px');
-               });
-               prevBtn.on('click', function() {
-                   if (prevSlide.length) {
-                       findReqPos(nextSlide);
-                       removeActive(nextSlide);
-                   } else {
-                       findReqPos(lastSlide);
-                       removeActive(prevSlide);
-                   }
-                   $this.css('left', '-=' + reqPos + 'px');
-               });
+               var activeSlide = $this.find('li.active');
+               console.log(activeSlide);
+
+               function moveActive(direction) {
+
+               }
            }
 
-           if (options.transition === 'fade') {
-               $this.wrap('<div id="slider-wrap__fade"></div>');
-               fade();
-           }
-
-           function slide() {
-               console.log(nextBtn);
-
-           }
-
-           function fade() {
-
-           }
-
-           function findReqPos(slide) {
-               reqPos = slide.offset().left - sliderOffset;
-           }
-           
-           function removeActive(reqSlide) {
-               reqSlide.addClass('active').siblings().removeClass('active');
-           }
        });
     };
 })(jQuery);
