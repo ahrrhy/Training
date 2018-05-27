7f8ab0978e2965e1c4cbb6bed6283dcf9063285c
ahrrhy <ahrrhy@gmail>
2018-05-27 04:50:37 +0300

    slide animation is ready

--- dest/js/build.js
+++ dest/js/build.js
@@ -10381,80 +10381,90 @@ return jQuery;
  * */
 ;(function ($) {
     $.fn.dvSlider = function (options) {
-       var defaults = {
+        var defaults = {
            speed : 2000,
            pause : 5000,
            transition : 'slide',
-           direction: 'forward',
+           direction: 'backward',
            controlPrev: '.previous-slide',
            controlNext: '.next-slide'
-       },
+        },
            options = $.extend(defaults, options),
            nextBtn = $(options.controlNext),
            prevBtn = $(options.controlPrev);
 
-       this.each(function () {
-           var $this = $(this);
-           if (options.transition === 'slide') {
-               var wrap = '<div id="slider-wrap__slide"></div>';
-               $this.wrap(wrap);
-               var currentSliderPosition = 0,
-                   items = function() { return $this.find('li'); },
-                   activeSlide = function() { return items().filter('.active')},
-                   nextSlide = function () { return activeSlide().next(); },
-                   prevSlide = function () { return activeSlide().prev() },
-                   firstSlide = function () { return items().first() },
-                   lastSlide = function () { return items().last()};
-               $this.width(nextSlide().width() * items().length);
+        this.each(function () {
+            var $this = $(this);
 
-               var start, stop;
+            if (options.transition === 'slide') {
+                var wrap = '<div id="slider-wrap__slide"></div>';
+                $this.wrap(wrap);
+
+                var currentSliderPosition = 0,
+                    items = function() { return $this.find('li'); },
+                    activeSlide = function() { return items().filter('.active')},
+                    nextSlide = function () { return activeSlide().next(); },
+                    prevSlide = function () { return activeSlide().prev() },
+                    firstSlide = function () { return items().first() },
+                    lastSlide = function () { return items().last()};
+                $this.width(nextSlide().width() * items().length);
+
+                var start;
+
+                autoSlide();
+
+                nextBtn.on('click', function() {
+                    clearInterval(start);
+                    nextSlideMove();
+                    autoSlide();
+                });
+                prevBtn.on('click', function() {
+                    clearInterval(start);
+                    prevSlideMove();
+                    autoSlide();
+                });
+
+                function autoSlide() {
+                    if (options.direction === 'forward') {
+                        start = setInterval(function () {
+                            nextSlideMove();
+                        }, options.pause);
+                    }
+                    if (options.direction === 'backward') {
+                        start = setInterval(function () {
+                            prevSlideMove();
+                        }, options.pause);
+                    }
+                }
 
 
-               nextBtn.on('click', function() {
-                   nextSlideMove();
-               });
-               prevBtn.on('click', function() {
-                   prevSlideMove();
-               });
+                function nextSlideMove() {
+                    console.log(1);
+                    if (nextSlide().length) {
+                        $this.animate({ right: currentSliderPosition + nextSlide().width() }, options.speed);
+                        currentSliderPosition += nextSlide().width();
+                        activeSlide().removeClass('active').next().addClass('active');
+                    } else {
+                        activeSlide().removeClass('active');
+                        firstSlide().addClass('active');
+                        $this.animate({ right: currentSliderPosition = 0 }, options.speed);
+                    }
+                }
 
-               function autoSlide() {
-                   if (options.direction === 'forward') {
-                       start = setInterval(nextSlideMove(), options.pause);
-                   }
-               }
+                function prevSlideMove() {
 
-
-               function nextSlideMove() {
-
-                   if (nextSlide().length) {
-                       $this.stop().animate({ right: currentSliderPosition + nextSlide().width() }, options.speed);
-                       currentSliderPosition += nextSlide().width();
-                       activeSlide().removeClass('active').next().addClass('active');
-
-                   } else {
-                       activeSlide().removeClass('active');
-                       firstSlide().addClass('active');
-                       $this.stop().animate({ right: currentSliderPosition = 0 }, options.speed);
-                   }
-               }
-
-               function prevSlideMove() {
-
-                   if (prevSlide().length) {
-                       console.log('done');
-                       $this.stop().animate({ right: currentSliderPosition - prevSlide().width() }, options.speed);
-                       currentSliderPosition -= prevSlide().width();
-                       activeSlide().removeClass('active').prev().addClass('active');
-                   } else {
-                       console.log(prevSlide().length);
-                       activeSlide().removeClass('active');
-                       lastSlide().addClass('active');
-                       $this.stop().animate({ right: currentSliderPosition = $this.width() - activeSlide().width() }, options.speed);
-                       console.log(prevSlide().length);
-                   }
-               }
-           }
-       });
+                    if (prevSlide().length) {
+                        $this.animate({ right: currentSliderPosition - prevSlide().width() }, options.speed);
+                        currentSliderPosition -= prevSlide().width();
+                        activeSlide().removeClass('active').prev().addClass('active');
+                    } else {
+                        activeSlide().removeClass('active');
+                        lastSlide().addClass('active');
+                        $this.animate({ right: currentSliderPosition = $this.width() - activeSlide().width() }, options.speed);
+                    }
+                }
+            }
+        });
     };
 })(jQuery);
 $(document).ready(function () {
